6c213908e9dc024b4482cd5bae6d9eb06f1afc64
dinomujki <dino_mujki@hotmail.com>
2015-10-19 03:50:56 -0500

    Wikipedia descriptions

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 from flask import Flask, render_template, request, url_for
 
 from bs4 import BeautifulSoup
-import urllib
+import urllib, urllib2
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -19,22 +19,50 @@ def form():
 # accepting: POST requests in this case
 @app.route('/List/', methods=['POST'])
 def List():
-    link=request.form['prompt']
-    url = "http://www.imdb.com/genre/" + str(link)
+    prompt=request.form['prompt']
+    url = "http://www.imdb.com/genre/" + str(prompt)
     html=urllib.urlopen(url).read()
     soup=BeautifulSoup(html)
     l = soup.find_all('div', class_='article')[0]
-    print l
-    # tbody = l.find_all('tbody')[0]
+    
+
     rawitems = l.find_all('td', class_="title")[0:10]
     names = []
-    descs= []
+    descs = []
+    imgs = []
+    counter = 0
     for item in rawitems:
-      names.append(str(item.find('a').contents[0]))
-      descs.append(str(item.find('span', class_='outline').get_text()))
+      name = item.find('a')
+      name['href']="http://www.imdb.com/"+name['href']
+      names.append(name)
+      
+      # names[counter]="http://www.imdb.com/"+names[counter]
+      googlenames = str(item.find('a').get_text())
+      googlenameslist = googlenames.split()
+      googlenames = "+".join(googlenameslist)
+      googleurl = "https://www.google.com/search?q=wikipedia+"+str(googlenames)+"+film+"+str(prompt)
+      
+      searchrequest = urllib2.Request(googleurl, None, {'User-Agent':'Mosilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11'})
+      urlfile = urllib2.urlopen(searchrequest)
+      page = urlfile.read()
+      
+      soup=BeautifulSoup(page)
+      wikiurl=soup.find('h3', class_="r")
+      wikiurl=wikiurl.find('a')
+      wikiurl=wikiurl['href']
+
+      deschtml=urllib.urlopen(wikiurl).read()
+      soup=BeautifulSoup(deschtml)
+      k=(soup.find_all('div', class_='mw-content-ltr')[0]).find('p')
+      descs.append(k)
+
+      img=(soup.find('div', class_='mw-content-ltr')).find('img')
+      imgs.append(img)
+      counter=counter+1
+      # descs.append(str(item.find('span', class_='outline').get_text()))
 
   
-    return render_template('form_action.html', prompt=link, name0=names[0], name1=names[1], name2=names[2],name3=names[3],name4=names[4],name5=names[5],name6=names[6],name7=names[7],name8=names[8],name9=names[9], desc0=descs[0], desc1=descs[1], desc2=descs[2], desc3=descs[3], desc4=descs[4], desc5=descs[5], desc6=descs[6], desc7=descs[7], desc8=descs[8], desc9=descs[9])
+    return render_template('form_action.html', prompt=prompt, image0 = imgs[0], name0=names[0], name1=names[1], name2=names[2],name3=names[3],name4=names[4],name5=names[5],name6=names[6],name7=names[7],name8=names[8],name9=names[9], desc0=descs[0], desc1=descs[1], desc2=descs[2], desc3=descs[3], desc4=descs[4], desc5=descs[5], desc6=descs[6], desc7=descs[7], desc8=descs[8], desc9=descs[9])
     # return render_template('form_action.html', name=rawitems)
 
 # Run the app :)
