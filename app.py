cc8b5ecb8d05b5f2373355c626241cf93e8cce6f
dinomujki <dino_mujki@hotmail.com>
2015-11-02 18:39:54 -0600

    n

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # and render_template, to render our templates (form and response)
 # we'll use url_for to get some URLs for the app on the templates
 from flask import Flask, render_template, request, url_for
-
+import re
 from bs4 import BeautifulSoup
 import urllib, urllib2
 
@@ -19,10 +19,20 @@ def form():
 # accepting: POST requests in this case
 @app.route('/List/', methods=['POST'])
 def List():
-    outputprompt=request.form['prompt']
+    inputprompt=request.form['prompt']
 
-    prompts = outputprompt.split()
+    prompts = inputprompt.split()
     prompt="+".join(prompts)
+
+    # youtubeurl = "https://www.youtube.com/results?search_query=documentary+top+"+str(prompt)
+    # # searchrequest = urllib2.Request(youtubeurl, None, {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_7_4) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11'})
+    # urlfile = urllib2.urlopen(youtubeurl)
+    # page = urlfile.read()
+    # soup = BeautifulSoup(page)
+    # # vidlink=soup.pretiffy()
+    # print soup
+    
+
 
     # use bing to find url of wikipedia list from prompt
     bingurl = "https://www.bing.com/search?q=wikipedia+top+ten+list+"+str(prompt)
@@ -30,18 +40,19 @@ def List():
     urlfile = urllib2.urlopen(searchrequest)
     page = urlfile.read()
     soup = BeautifulSoup(page)
-    k=soup.find('ol', id="b_results").a['href']
-    wikiurl=k
+    wikiurl=soup.find('ol', id="b_results").a['href']
     print wikiurl
+
 
     # scrape wiki url
     html=urllib.urlopen(wikiurl).read()
     soup=BeautifulSoup(html)
 
     l = soup.find('table', class_='wikitable')
+    
     # l = soup.find('table')
     # print l
-
+    general=soup.find_all('p')[0:3]
     # rawitems = l.find_all('th', {'scope':'row'})[0:10]
     rawitems = l.find_all('tr')[0:15]
     # print rawitems
@@ -60,10 +71,15 @@ def List():
                 linkitem = scoperow[0].find('a', class_=lambda x: x != 'reference')
             else:
                 linkitem = item.find('a')
+                # data=item.find('td')
+            # print "data = %s" %data
             print linkitem
             name = linkitem.contents[0]
+
             linkurl = linkitem['href']
             print name
+
+            
             #   name['href']="http://www.imdb.com/"+name['href']
             names.append(name)
 
@@ -87,17 +103,18 @@ def List():
             deschtml=urllib.urlopen(wikiurl).read()
             soup=BeautifulSoup(deschtml)
             # k=(soup.find_all('div', class_='mw-content-ltr')[0]).find('p')
-            k=soup.find_all('div', class_='mw-content-ltr')[0].find('p', recursive=False)
+            k=soup.find_all('div', class_='mw-content-ltr')[0].find_all('p', recursive=False)[0]
             descs.append(k)
 
-            img=(soup.find('div', class_='mw-content-ltr')).find('img')
+            # img=soup.find('   div', class_='mw-content-ltr'    ).find('img')
+            img=soup.find('div', class_='mw-content-ltr').find('img', {'src' : re.compile(r'(jpe?g)$')})
             imgs.append(img)
 
             counter=counter+1
             #   descs.append(str(item.find('span', class_='outline').get_text()))
 
 
-    return render_template('form_action.html', prompt=outputprompt, image0 = imgs[0],image1 = imgs[1],image2 = imgs[2],image3 = imgs[3],image4 = imgs[4],image5 = imgs[5],image6 = imgs[6],image7 = imgs[7],image8 = imgs[8],image9 = imgs[9], name0=names[0], name1=names[1], name2=names[2],name3=names[3],name4=names[4],name5=names[5],name6=names[6],name7=names[7],name8=names[8],name9=names[9], desc0=descs[0], desc1=descs[1], desc2=descs[2], desc3=descs[3], desc4=descs[4], desc5=descs[5], desc6=descs[6], desc7=descs[7], desc8=descs[8], desc9=descs[9])
+    return render_template('form_action.html', prompt=inputprompt, image0 = imgs[0],image1 = imgs[1],image2 = imgs[2],image3 = imgs[3],image4 = imgs[4],image5 = imgs[5],image6 = imgs[6],image7 = imgs[7],image8 = imgs[8],image9 = imgs[9], name0=names[0], name1=names[1], name2=names[2],name3=names[3],name4=names[4],name5=names[5],name6=names[6],name7=names[7],name8=names[8],name9=names[9], desc0=descs[0], desc1=descs[1], desc2=descs[2], desc3=descs[3], desc4=descs[4], desc5=descs[5], desc6=descs[6], desc7=descs[7], desc8=descs[8], desc9=descs[9], general1 =general[0], general2 =general[1], general3 =general[2])
     # return render_template('form_action.html', prompt=prompt,name0=names[0], name1=names[1], name2=names[2],name3=names[3],name4=names[4],name5=names[5],name6=names[6],name7=names[7],name8=names[8],name9=names[9], desc0=descs[0], desc1=descs[1], desc2=descs[2], desc3=descs[3], desc4=descs[4], desc5=descs[5], desc6=descs[6], desc7=descs[7], desc8=descs[8], desc9=descs[9])
     # return render_template('form_action.html', prompt=prompt,name0=names[0], name1=names[1], name2=names[2],name3=names[3],name4=names[4],name5=names[5],name6=names[6],name7=names[7],name8=names[8],name9=names[9])
     # return render_template('form_action.html', prompt=prompt)
